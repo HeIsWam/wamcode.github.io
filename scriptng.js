46329a4d0b36e47fb9fbc9b1eff308552cdd3b6b
HeIsWam <87457578+HeIsWam@users.noreply.github.com>
2021-07-23 19:14:02 +0530

    Add files via upload

---
+++ scriptng.js
@@ -0,0 +1,13 @@
+function showMenu() {
+  var x = document.getElementById("nav-bar")
+  x.style.display = "block";
+  var y = document.getElementById("close-menu-bar")
+  y.style.display = "block";
+}
+function closeMenu() {
+  var x = document.getElementById("nav-bar")
+  x.style.display = "none";
+  var y = document.getElementById("close-menu-bar")
+  y.style.display = "none";
+}
+
